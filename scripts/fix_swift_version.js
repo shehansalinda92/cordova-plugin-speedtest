a4ad7e5748961188736dae19fdd8b65768b13735
shehan <shehan.salinda@ncinga.net>
2024-11-20 20:31:42 +0530

    change hook path

--- scripts/fix_swift_version.js
+++ scripts/fix_swift_version.js
@@ -1,5 +1,6 @@
-// fix_swift_version.js
 module.exports = function (ctx) {
+    console.log('Running fix_swift_version.js');
+
     const fs = require('fs');
     const path = require('path');
 
@@ -10,5 +11,7 @@ module.exports = function (ctx) {
     if (fs.existsSync(configPath)) {
         fs.appendFileSync(configPath, '\nUSE_SWIFT_LANGUAGE_VERSION=5.0\n');
         console.log('USE_SWIFT_LANGUAGE_VERSION set to 5.0 in build.xcconfig');
+    } else {
+        console.log('build.xcconfig not found');
     }
 };
